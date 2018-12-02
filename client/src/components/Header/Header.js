2a9572de4e89abcd7f5aa0cc00801a60cd3694a6
EJL <EvanJLong11@gmail.com>
2018-12-02 14:13:01 -0600

    homepage pretty much donezo, need page routing

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,13 +3,14 @@ import '../Header/Header.css'
 import { Link } from 'react-router-dom';
 import {Jumbotron, Button} from 'react-bootstrap'
 import Login from '../Login/Login';
+import logo from '../../assets/images/logo_2.png'
 
 const Header = () => (
   <div>
 <Jumbotron id="jumbotronz">
-  <h1>Bid & Build</h1>
+  <h1><img id="logoz" src={logo} alt={"logo"}/> Bid & Build</h1>
   <p>
-    Start winning more construction bids in the DFW area now.  
+    Start winning more construction bids in the DFW area today!  
     </p>
     <p>
     Login or Sign-Up to get started.
